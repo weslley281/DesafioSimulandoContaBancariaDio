5a0595308407a075e6c9b0d7867fa41c81d0b45e
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-08-23 06:27:07 -0400

    a

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,11 @@
 public class ContaTerminal {
+    public static void main(String[] args) {
+        //importar a classe scanner
+
+        //exibirs as mensagens para usuario
+
+        //obiter pela classe scanner os valores digitados no terminal
+
+        //exibir a mensagem final da conta criada
+    }
 }
